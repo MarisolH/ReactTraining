cd65b8a737634261296da7004ad6bc3b496caf65
Javier Herrera Gonzalez <jherrerag@cotemar.com.mx>
2020-01-21 17:09:56 -0600

    release() added code into my screen

--- src/screens/JaviScreen/JaviScreen.js
+++ src/screens/JaviScreen/JaviScreen.js
@@ -2,7 +2,10 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid';
 
 import MenuBar from '../../components/MenuBar/MenuBar';
+import CardComponent from '../../components/CardComponent/CardComponent';
+import TextInput from '../../components/TextInput/TextInput';
 // import Typography from '@material-ui/core/Typography';
+
 import { useStyles } from './JaviScreenStyles';
 
 export default function JaviScreen(props) {
@@ -27,7 +30,27 @@ export default function JaviScreen(props) {
             alignItems="center"
             className={classes.rootContainer}
         >
-                Aquí irá tu código J
+
+            <Grid
+                container
+                direction="column"
+                justify="top"
+                alignItems="left"
+                className={classes.rootContainer}
+            >
+                {/* <CardComponent title={"hola"}> */}
+                    <Grid item xs={6} md={5} lg={5} sm container >
+                        <img alt='' className={classes.illustration} src={require('../../assets/javier.png')}></img>
+                    </Grid>
+                    <Grid item xs={6} md={5} lg={5} sm container >
+                        <TextInput label={"Nombre"}></TextInput>
+                    </Grid>
+                    <Grid item xs={6} md={5} lg={5} sm container >
+                        <TextInput label={"Apellido"}></TextInput>
+                    </Grid>
+                {/* </CardComponent> */}
+            </Grid>  
+            
 
         </Grid>
     </React.Fragment>
