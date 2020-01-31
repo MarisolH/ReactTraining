51a956439731dd015228542d550ce95a7874d0b8
Rodolfo Morelos Romero <rmorelosr@cotemar.com.mx>
2020-01-31 08:51:15 -0600

    release(cardcomponent modified)

--- src/components/CardComponent/CardComponent.js
+++ src/components/CardComponent/CardComponent.js
@@ -1,9 +1,12 @@
-import React from 'react';
+//import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import ModalComponent from '../../components/ModalComponent/ModalComponent'
+import React, { useState }  from 'react'
+
 
 import { useStyles } from './CardComponentStyles'
 export default function CardComponent(props) {
@@ -17,8 +20,18 @@ export default function CardComponent(props) {
                 , Campodeestudio: 'Campo de estudio'
                 , AñoFinal: 'Año final'
             }
-        ]
+        ]/*,
+        ListaDeValues:{ 
+            setEscuela
+            , setLicenciatura
+            , setCampodeestudio
+            , setAñoFinal
+        }*/
     }
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [direccion, setDireccion] = useState('');
 
     return <Grid
         container
@@ -42,20 +55,23 @@ export default function CardComponent(props) {
                         justify='center'
                         alignItems='flex-end'>
                         <Typography component="h1" align='center' className={classes.title} container  >
-                            {text.listaDeLabel[0].titulo}
+                            {text.listaDeLabel[0].titulo} 
                         </Typography>
                         <Grid item xs={12}>
-                            <TextField className={classes.pos} label={text.listaDeLabel[0].Escuela}></TextField>
+                            <TextField className={classes.pos} label={"Nombre"} setValue={setNombre} ></TextField>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField className={classes.pos} label={text.listaDeLabel[0].Licenciatura} ></TextField>
+                            <TextField className={classes.pos} label={text.listaDeLabel[0].Licenciatura} /*setValue={text.ListaDeValues[0].setLicenciatura}*/></TextField>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField className={classes.pos} label={text.listaDeLabel[0].Campodeestudio} ></TextField>
+                            <TextField className={classes.pos} label={text.listaDeLabel[0].Campodeestudio} /*setValue={text.ListaDeValues[0].setCampodeestudio}*/></TextField>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField className={classes.pos} label={text.listaDeLabel[0].AñoFinal}></TextField>
+                            <TextField className={classes.pos} label={text.listaDeLabel[0].AñoFinal} /*setValue={text.ListaDeValues[0].setAñoFinal}*/></TextField>
                         </Grid>
+                        <Grid>
+                    <ModalComponent title={'Guardar'} buttonText={'Guardar'} nombreusuario={nombre} /*apellidousuario={text.ListaDeValues[0].Licenciatura} telefonousuario={text.ListaDeValues[0].Campodeestudio} direccionusuario={text.ListaDeValues[0].AñoFinal}*/confirmText={'Cerrar'}/>
+                    </Grid>
                     </Grid>
                 </Grid>
 
